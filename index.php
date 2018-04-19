116be6cc7d9137b1ce780d662e234899e1bae12e
locdd <locddspkt@gmail.com>
2018-04-19 17:05:42 +0700

    first submit clock

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: locdd
+ * Date: 4/19/18
+ * Time: 16:49
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>    <title>Icon list</title>
+</head>
+<body style="padding: 20px">
+    <?php include_once 'icons/clock_plus.html' ?>
+</body>
+</html>
